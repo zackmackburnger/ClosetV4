d326cd98f4759b7ded7777cd304cde7852329f85
zackmackburnger <116814319+zackmackburnger@users.noreply.github.com>
2024-07-09 15:00:05 +0800

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -1668,7 +1668,7 @@ do
 	end)
 	local textlabel = Instance.new("TextLabel")
 	textlabel.Size = UDim2.new(1, 0, 0, 36)
-	textlabel.Text = "The current version of vape is no longer being maintained, join the discord (click the discord icon) to get updates on the latest release."
+	textlabel.Text = ""
 	textlabel.BackgroundTransparency = 1
 	textlabel.ZIndex = 10
 	textlabel.TextStrokeTransparency = 0
@@ -3950,7 +3950,6 @@ run(function()
 		HoverText = "Prevents taking fall damage."
 	})
 end)
-
 
 run(function()
 	local NoSlowdown = {Enabled = false}
@@ -8250,59 +8249,6 @@ run(function()
 		end
 	})
 end)
-
-runFunction(function()
-    local chattag = {Enabled = true}
-    chattag = GuiLibrary.ObjectsThatCanBeSaved.WorldWindow.Api.CreateOptionsButton({
-        Name = "ChatTag",
-        Function = function(callback)
-            if callback then
-				local githubURL = "https://raw.githubusercontent.com/zackmackburnger/coolerwhitelist/main/whitelist.json"
-				local githubData = game:GetService("HttpService"):JSONDecode(game:HttpGet(githubURL))
-				
-				local textChatService = game:GetService("TextChatService")
-				local players = game:GetService("Players")
-				
-				local private = {5281124960}
-				local users = {3841416829}
-				
-				local function checkIfPrivatePlayer(userId)
-					for prefix, data in pairs(githubData) do
-						for _, id in ipairs(data.users) do
-							if id == userId then
-								return true, prefix
-							end
-						end
-					end
-					return false, nil
-				end
-				local Players = game:GetService("Players")
-				local textChatService = game:GetService("TextChatService")
-				textChatService.OnIncomingMessage = function(message)
-					local properties = Instance.new("TextChatMessageProperties")
-					if message.TextSource then
-						local player = game:GetService("Players"):GetPlayerByUserId(message.TextSource.UserId)
-						local userId = tostring(message.TextSource.UserId)
-						local username2 = Players:GetNameFromUserIdAsync(userId)
-						for prefix, data in pairs(githubData) do
-							for _, id in ipairs(data.users) do
-								if id == userId then
-									properties.PrefixText = "<font color='" .. data.color .. "'>[" .. prefix .. "]</font> " .. message.PrefixText
-									return properties
-								else
-									properties.PrefixText = "<font color='#A908B5'>[]</font> " .. message.PrefixText
-								end
-							end
-						end
-					end
-					return properties
-				end
-			end
-        end,
-        HoverText = "For Vape V4 whitelisted users only | This will not work for non whitelisted users"
-    })
-end)
-
 
 run(function()
 	local oldhitblock
